291ba33b3123bd1e054bf65ccc54c926e8e25989
Mayra <mayra.cademartori@gmail.com>
2021-03-14 22:19:25 -0300

    Feat(Schedule Service)

---
+++ src/services/ScheduleService.ts
@@ -0,0 +1,25 @@
+export default class ScheduleService {
+
+  private static HOURS_TO_POST = [10, 22] as const
+  private static day = new Date().getDay() 
+  private static schedule = [...ScheduleService.HOURS_TO_POST]
+
+  static get isItTime () {
+    const date = new Date()
+    ScheduleService.resetDay(date)
+    const index = ScheduleService.schedule.findIndex(e => date.getHours() >= e)
+    if(index >= 0) {
+      ScheduleService.schedule.splice(index, 1)
+      return true
+    }
+    return false
+  }
+
+  private static resetDay = (date: Date) => {
+    const currDay = date.getDay()
+    if(ScheduleService.day != currDay) {
+      ScheduleService.day = currDay
+      ScheduleService.schedule = [...ScheduleService.HOURS_TO_POST]
+    }
+  }
+}
