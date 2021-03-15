4f02ea106d385b00317d8d531c16fa2b390bf801
Mayra <mayra.cademartori@gmail.com>
2021-03-15 15:31:17 -0300

    Get Schedule

--- src/services/ScheduleService.ts
+++ src/services/ScheduleService.ts
@@ -1,13 +1,13 @@
 export default class ScheduleService {
 
-  private static HOURS_TO_POST = [16, 17] //GMT +00:00, conversão pra BR é -3 
+  private static HOURS_TO_POST = [19, 20] //GMT +00:00, conversão pra BR é -3 
   private static day = new Date().getDay() 
   private static schedule = ScheduleService.HOURS_TO_POST
 
   static get isItTime () {
     const date = new Date()
     ScheduleService.resetDay(date)
-    const index = ScheduleService.schedule.findIndex(e => date.getHours() >= e)
+    const index = ScheduleService.schedule.findIndex(e => date.getHours() === e)
     if(index >= 0) {
       ScheduleService.schedule.splice(index, 1)
       return true
@@ -23,8 +23,11 @@ export default class ScheduleService {
     }
   }
 
+  static getSchedule = () => `-> Hours To Post: ${ScheduleService.HOURS_TO_POST} Schedule: ${ScheduleService.schedule}`
+
   static setSchedule = (newSchedule: number[] ) => {
     ScheduleService.HOURS_TO_POST = newSchedule
+    ScheduleService.schedule = ScheduleService.HOURS_TO_POST
     return `-> New Schedule: ${ScheduleService.HOURS_TO_POST}`
   }
 }
