82e29273fe5e16fc14677a11fdf1bd5917394246
Mayra <mayra.cademartori@gmail.com>
2021-03-24 19:52:58 -0300

    Finishing up

--- src/services/QuoteService.ts
+++ src/services/QuoteService.ts
@@ -11,23 +11,19 @@ export default class QuoteService {
 
 		if(forcePop) path += 'force-pop'
 
-		let thread: string[] = []
-
 		superagent.put(path)
 		.set('Authorization', `Bearer ${process.env.BEARER_TOKEN}`)
 		.set('Accept', 'application/json')
 		.end((err, res) => {
 			if (res.ok) {
 				if(res.status === 200) {
-					thread = QuoteService.getThread(res.body)
+					const thread = QuoteService.getThread(res.body)
 					QuoteService.tweetThread(thread)
 				} 
 				else console.error(`-> ERROR STATUS ${res.status}`)
 			} 
 			else console.error(err)
 		})
-
-		return thread
 	}
 
 	private static getThread = (quote: QuoteDataModel): string[] => {
