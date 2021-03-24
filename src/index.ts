ae710ea9806c4537e28aab2da3ce98e84c9af966
Mayra <mayra.cademartori@gmail.com>
2021-03-24 13:48:15 -0300

    Fix(not used import)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express"
 import QuoteService from "./services/QuoteService"
-import ScheduleService from "./services/ScheduleService"
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -8,18 +7,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-	return res.send(
-		ScheduleService.isItTime 
-		? QuoteService.popQuote() 
-		: 'Not Time Yet!'
-	)
+	return res.send(QuoteService.popQuote())
 })
 
-app.get('/force-pop', (req, res) => {
-	const auth = req.headers.authorization
-	if (!auth || auth.substring(7) !== process.env.BEARER_TOKEN) {
-    return res.status(403).json({ error: 'Credentials are incorrect!' })
-  } else res.send(QuoteService.popQuote())
-})
+// app.get('/force-pop', (req, res) => {
+// 	const auth = req.headers.authorization
+// 	if (!auth || auth.substring(7) !== process.env.BEARER_TOKEN) {
+//     return res.status(403).json({ error: 'Credentials are incorrect!' })
+//   } else res.send(QuoteService.popQuote())
+// })
 		
 app.listen(PORT, () => console.log(`\n-> Server is running at PORT: ${PORT}`))
