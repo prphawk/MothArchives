7b44b0e5ea37e777ef64201002e38c51a0f175f7
Mayra <mayra.cademartori@gmail.com>
2021-03-25 22:36:35 -0300

    Feat(avoid unnecessary api pings)

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-	return res.send(QuoteService.popQuote())
+
+	const isItTime = () => {
+		const now = new Date().getHours()
+		return [11, 17, 23].some(h => h == now) //GMT
+	} 
+
+	return res.send(isItTime() ? QuoteService.popQuote() : 'Not Time Yet!')
 })
 
 app.get('/force-pop', (req, res) => {
