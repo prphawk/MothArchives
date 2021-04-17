468bd66a2e38654a0560dba1c6d682151843815e
Mayra <mayra.cademartori@gmail.com>
2021-04-17 17:57:31 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import cron from 'node-cron'
 
 const PORT = process.env.PORT || 8000
 
-//import express from "express"
+import express from "express"
 
-// const app = express()
+const app = express()
 
-// app.get('/force-pop', (req, res) => {
-// 	return res.send(TweetService.tweetQuote(true))
-// })
+app.get('/force-pop', (req, res) => {
+	return res.send(TweetService.tweetQuote(true))
+})
 		
-// app.listen(PORT, () => console.log(`-> Server is running at PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`-> Server is running at PORT: ${PORT}`))
 
 console.log(`-> v.0.4 ${new Date()} Server is running at PORT: ${PORT}`)
 
