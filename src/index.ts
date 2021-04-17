619d538c4e1a8876a5c5a7536ef5edbeaef8319d
Mayra <mayra.cademartori@gmail.com>
2021-04-17 17:41:25 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-//import express from "express"
+import express from "express"
 import ReplyService from "./service/ReplyService"
 import TweetService from "./service/TweetService"
 import cron from 'node-cron'
 
-//const app = express()
+const app = express()
 const PORT = process.env.PORT || 8000
 
-// app.get('/force-pop', (req, res) => {
-// 	return res.send(TweetService.tweetQuote(true))
-// })
+app.get('/force-pop', (req, res) => {
+	return res.send(TweetService.tweetQuote(true))
+})
 		
-// app.listen(PORT, () => console.log(`-> Server is running at PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`-> Server is running at PORT: ${PORT}`))
 
 console.log(`-> v.0.3 ${new Date()} Server is running at PORT: ${PORT}`)
 
